312fcecb77eb9012ba7823640139c4d91313a97e
sromeiro <eromeiro@mail.usf.edu>
2017-09-14 17:06:29 -0400

    Compiles and runs

--- Project01/processes.c
+++ Project01/processes.c
@@ -35,7 +35,7 @@ void process4();
 
 int main()
 {
-  int shmid, pid1 = 100, pid2 = 100, pid3 = 100, pid4 = 100, ID, status;
+  int shmid, pid1, pid2, pid3, pid4, ID, status;
   char *shmadd;
   shmadd = (char *) 0;
 
@@ -51,89 +51,54 @@ int main()
   }
 
   total->value = 0;
-/*
+
+  //Create process fork and run process1 only if it's child process
   if ((pid1 = fork()) == 0)
   {
     process1();
-  }
-*/
 
-  //printf("Before fork my pid is %d and pid1 is %d\n", getpid(), pid1);
-  pid1 = fork();
-  //printf("After fork my pid is %d and pid1 is %d\n", getpid(), pid1);
-
-  if(pid1 == 0)
-  {
-    //We are the child process
-    printf("I am the child process from pid1 with ID %d\n", getpid());
-    printf("Processing...\n");
-    process1();
-  }
-  else
-  {
-    //We are the parent process
-    printf("I am the parent process from pid1 with ID %d\n", getpid());
-    printf("Waiting on child...\n");
-    wait(NULL);
-    printf("Child process from pid1 has finished\n");
   }
 
-/*
-  if(pid2 == 0)
+  //Create process fork and run process2 only if it's child process
+  if (pid1 != 0 && (pid2 = fork()) == 0)
   {
-    //We are the child process
-    printf("I am the child process from pid2 with ID %d\n", getpid());
-    printf("Processing...\n");
     process2();
   }
-  else if(pid2 != 100)
-  {
-    //We are the parent process
-    printf("I am the parent process from pid2 with ID %d\n", getpid());
-    printf("Waiting on child...\n");
-    wait(NULL);
-    printf("Child process from pid2 has finished\n");
-    pid3 = fork(); //Create a new fork with pid3
-  }
 
-  if(pid3 == 0)
+  //Create process fork and run process3 only if it's child process
+  if (pid1 != 0 && pid2 != 0 && (pid3 = fork()) == 0)
   {
-    //We are the child process
-    printf("I am the child process from pid3 with ID %d\n", getpid());
-    printf("Processing...\n");
     process3();
   }
-  else if(pid3 != 100)
-  {
-    //We are the parent process
-    printf("I am the parent process from pid3 with ID %d\n", getpid());
-    printf("Waiting on child...\n");
-    wait(NULL);
-    printf("Child process from pid3 has finished\n");
-    pid4 = fork(); //Create a new fork with pid4
-  }
 
-  if(pid4 == 0)
+  //Create process fork and run process4 only if it's child process
+  if (pid1 != 0 && pid2 !=0 && pid3 != 0 && (pid4 = fork()) == 0)
   {
-    //We are the child process
-    printf("I am the child process from pid4 with ID %d\n", getpid());
-    printf("Processing...\n");
     process4();
   }
-  else if(pid4 != 100)
+
+  //Waits for each process to end and prints the correct one
+  pid1 = wait(&status);
+  pid2 = wait(&status);
+  pid3 = wait(&status);
+  pid4 = wait(&status);
+
+  if(pid1 > 0 && pid2 > 0 && pid3 > 0 && pid4 > 0)
   {
-    //We are the parent process
-    printf("I am the parent process from pid4 with ID %d\n", getpid());
-    printf("Waiting on child...\n");
-    wait(NULL);
-    printf("Child process from pid4 has finished\n");
+    printf(" Child process %d has ended\n", pid1);
+    printf(" Child process %d has ended\n", pid2);
+    printf(" Child process %d has ended\n", pid3);
+    printf(" Child process %d has ended\n", pid4);
   }
-*/
 
+  if (shmdt(total) == -1)
+    {
+      perror ("shmdt");
+      exit (-1);
+    }
 
+  shmctl(shmid, IPC_RMID, NULL);
 
-
-  printf("\nProgram terminated\n\n");
   //Successful program termination
   return 0;
 }
@@ -142,36 +107,32 @@ int main()
 
 void process1()
 {
-  printf("Process 1 running\n");
-  while(total->value < 100000)
-    total->value += 1;
-  //sleep(5);
+  int i;
+  for(i = 0; i < 100000; i++)
+    total->value++;
   printf("Value is %d\n", total->value);
 }
 
 void process2()
 {
-  printf("Process 2 running\n");
-  while(total->value < 200000)
-    total->value += 1;
-  //sleep(5);
+  int i;
+  for(i = 0; i < 200000; i++)
+    total->value++;
   printf("Value is %d\n", total->value);
 }
 
 void process3()
 {
-  printf("Process 3 running\n");
-  while(total->value < 300000)
-    total->value += 1;
-  //sleep(5);
+  int i;
+  for(i = 0; i < 300000; i++)
+    total->value++;
   printf("Value is %d\n", total->value);
 }
 
 void process4()
 {
-  printf("Process 4 running\n");
-  while(total->value < 500000)
-    total->value += 1;
-  //sleep(5);
+  int i;
+  for(i = 0; i < 500000; i++)
+    total->value++;
   printf("Value is %d\n", total->value);
 }
